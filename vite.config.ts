94b53048c8139ae11bb5285e7abff5f4d853f1ca
unicornlab-brylleg <brylle@cxunicorn.com>
2022-06-02 18:20:41 +0400

    Added chunking config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { dependencies } from './package.json';
+
+const packages = ['react', 'react-dom'];
+
+function renderChunks(deps: Record<string, string>) {
+  const chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (packages.includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    reportCompressedSize: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: packages,
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
 });
