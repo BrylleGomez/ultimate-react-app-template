4d7f0ac9d28b1b3eaf60d85c9fc3ef053da87b00
unicornlab-brylleg <brylle@cxunicorn.com>
2022-06-03 16:40:43 +0400

    Updated eslint config and vite config checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
 import { dependencies } from './package.json';
 
 const packages = ['react', 'react-dom'];
@@ -16,7 +17,15 @@ function renderChunks(deps: Record<string, string>) {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      eslint: {
+        files: ['./src'],
+        extensions: ['.tsx', '.ts'],
+      },
+    }),
+  ],
   build: {
     reportCompressedSize: true,
     rollupOptions: {
